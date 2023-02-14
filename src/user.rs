652df63a2f6ebf9962e63eff4fdc14fcc5e91ee1
Turakar <turakar23@gmail.com>
2023-02-14 19:11:01 +0100

    Add impressum

--- src/user.rs
+++ src/user.rs
@@ -471,11 +471,7 @@ pub async fn waiting_list_notify(
             .from(config.email_from_address.parse()?)
             .subject(
                 LOCALES
-                    .lookup_single_language(
-                        &lang.parse()?,
-                        "waiting-list",
-                        Some(&mail_header_args),
-                    )
+                    .lookup_single_language(&lang.parse()?, "waiting-list", Some(&mail_header_args))
                     .ok_or_else(|| anyhow!("Missing translation for waiting-list!"))?,
             )
             .header(header::ContentType::TEXT_PLAIN)
@@ -487,4 +483,15 @@ pub async fn waiting_list_notify(
         mailer.send(message).await?;
     }
     Ok(())
+}
+
+#[get("/impressum")]
+pub async fn impressum_get(lang: Language, config: &State<Config>) -> Template {
+    Template::render(
+        "impressum",
+        context! {
+            lang: lang.into_string(),
+            impressum: config.impressum.as_str()
+        },
+    )
 }
